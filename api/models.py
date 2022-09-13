b27485349931556d7e2e0acd1e32560dc226ac41
INGANZAMARUMPU <jonkur96@gmail.com>
2022-09-13 19:51:16 +0200

    done with models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Niveau(models.Model):
+	id = models.AutoField(primary_key=True)
+	nom = models.CharField(max_length=32)
+	poid = models.FloatField()
+
+class Employee(models.Model):
+	id = models.AutoField(primary_key=True)
+	nom = models.CharField(max_length=32)
+	prenom = models.CharField(max_length=32)
+	matricule = models.CharField(max_length=16)
+	niveau = models.ForeignKey(Niveau, on_delete=models.PROTECT)
+
+class Presence(models.Model):
+	id = models.BigAutoField(primary_key=True)
+	date = models.DateField(auto_now_add=True)
+	employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+	time_in = models.DateTimeField(auto_now_add=True, editable=False)
+	time_out = models.DateTimeField(null=True, editable=False)
+
+class Conge(models.Model):
+	id = models.AutoField(primary_key=True)
+	employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+	debut = models.DateField()
+	fin = models.DateField()
+	payee = models.BooleanField(default=True)
