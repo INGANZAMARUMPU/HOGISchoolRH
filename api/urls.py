2614b1d9c9819f0aceabfc0b058f7595d562316a
ArtcalO <niyca14@gmail.com>
2022-09-17 07:59:36 +0200

    add viewsets and urls

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,15 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from .views import *
+
+router = routers.DefaultRouter()
+
+
+router.register('niveau', NiveauViewSet)
+router.register('employee', EmployeeViewSet)
+router.register('presence', PresenceViewSet)
+router.register('conge', CongeViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import *
+from .serializers import *
 
-# Create your views here.
+
+class NiveauViewSet(viewsets.ModelViewSet):
+    queryset = Niveau.objects.all()
+    serializer_class = NiveauSerializer
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+class PresenceViewSet(viewsets.ModelViewSet):
+    queryset = Presence.objects.all()
+    serializer_class = PresenceSerializer
+
+class CongeViewSet(viewsets.ModelViewSet):
+    queryset = Conge.objects.all()
+    serializer_class = CongeSerializer
