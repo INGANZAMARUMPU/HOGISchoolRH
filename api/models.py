fae718d22267c549f62f2283d9233d38d9e03e22
INGANZAMARUMPU <jonkur96@gmail.com>
2022-09-20 19:22:12 +0200

    permissions, authetication, pagination

--- api/models.py
+++ api/models.py
@@ -16,12 +16,15 @@ class Employee(models.Model):
 	matricule = models.CharField(max_length=16)
 	niveau = models.ForeignKey(Niveau, on_delete=models.PROTECT)
 
+	def __str__(self):
+		return f"{self.nom} {self.prenom}"
+
 class Presence(models.Model):
 	id = models.BigAutoField(primary_key=True)
 	date = models.DateField(auto_now_add=True)
 	employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
 	time_in = models.DateTimeField(auto_now_add=True, editable=False)
-	time_out = models.DateTimeField(null=True, editable=False)
+	time_out = models.DateTimeField(null=True)
 
 class Conge(models.Model):
 	id = models.AutoField(primary_key=True)
--- api/views.py
+++ api/views.py
@@ -1,20 +1,31 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
+from rest_framework.authentication import SessionAuthentication
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
 from .models import *
 from .serializers import *
-
 
 class NiveauViewSet(viewsets.ModelViewSet):
     queryset = Niveau.objects.all()
     serializer_class = NiveauSerializer
+    permission_classes = [IsAdminUser]
+    authentication_classes = [JWTAuthentication, SessionAuthentication]
 
 class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+    permission_classes = [IsAdminUser]
+    authentication_classes = [JWTAuthentication, SessionAuthentication]
 
 class PresenceViewSet(viewsets.ModelViewSet):
     queryset = Presence.objects.all()
     serializer_class = PresenceSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication, SessionAuthentication]
 
 class CongeViewSet(viewsets.ModelViewSet):
     queryset = Conge.objects.all()
     serializer_class = CongeSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    authentication_classes = [JWTAuthentication, SessionAuthentication]
